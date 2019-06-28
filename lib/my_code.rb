0305117af70d0356d2360bccee9d7041a34d3258
Marco Frisina <marco@frisina.xyz>
2019-06-28 10:18:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,5 +19,9 @@ def reduce(source_array, starting_point=0)
     starting_point = yield(starting_point, source_array[i])
     i += 1
   end
+  if starting_point = 0
+    return true
+  else return starting_point
+  end
   return starting_point
 end
