99f3276d465c91172d08c09d01476b192fe93104
Marco Frisina <marco@frisina.xyz>
2019-06-28 10:24:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,11 +15,6 @@ end
 def reduce(source_array, starting_point=0)
   i = 0
 
-  if starting_point
-    return true
-  else return starting_point
-  end
-
   while i < source_array.length do
     starting_point = yield(starting_point, source_array[i])
     i += 1
