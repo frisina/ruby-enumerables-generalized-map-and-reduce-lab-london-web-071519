29d174be0aa4d09266f16a3d3d59043518fd782c
Marco Frisina <marco@frisina.xyz>
2019-06-28 10:05:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,15 +12,15 @@ newArray = [];
 return newArray
 end
 
-def reduce(source_array, starting_point=0)
-  i = 0
-  returnValue = 0
+def reduce_to_any_true(source_array)
+  i = 0;
+  returnValue = false
 
-if starting_point
-  returnValue = source_array.inject(yield(starting_point))
-else
-  returnValue = yield(source_array.inject)
-end
-return returnValue
-
+  while i < source_array.length do
+    if (!!source_array[i] == true)
+      returnValue = true
+    end
+      i += 1
+  end
+  return returnValue
 end
